b4b4b20d1025c4356f8159b76b172c01679c4327
MdSadman39 <shaon.cse.pust@gmail.com>
2025-08-13 01:07:18 +0600

    string

---
+++ 09_string_functions.py
@@ -0,0 +1,9 @@
+name= "sadman"
+
+print(len(name))
+print(name.endswith("man"))
+print(name.startswith("sad"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.title()) #Capitalizes first letter of each word
